2773423638e3920e5ec0f94c91395949c993e6a5
Sekaivi <rouibahmayssa@gmail.com>
2025-04-03 11:51:15 +0200

    oki

--- views/Base.php
+++ views/Base.php
@@ -35,7 +35,62 @@
       <button>Langue</button>
     </div>
   </nav>
-  </header>
+  <!-- Bordure sous le header composée de 4 segments -->
+  <div class="header-border">
+    <div class="border-segment" style="background-color: #FFE009;"></div>
+    <div class="border-segment" style="background-color: #31FF6E;"></div>
+    <div class="border-segment" style="background-color: #FF9AF0;"></div>
+    <div class="border-segment" style="background-color: #3C74FF;"></div>
+  </div>
+</header>
+
+
+<aside id="sidebar">
+  <ul>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=1">
+        <img src="public/images/Picto_TV.png" alt="TV Shows">
+        <span>TV Shows</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=2">
+        <img src="public/images/Picto_Music.png" alt="Music">
+        <span>Music</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=3">
+        <img src="public/images/Picto_Cinema.png" alt="Cinéma">
+        <span>Cinéma</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=4">
+        <img src="public/images/Picto_Gaming.png" alt="Video Games">
+        <span>Video Games</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=5">
+        <img src="public/images/Picto_Books.png" alt="Books & Comics">
+        <span>Books & Comics</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=6">
+        <img src="public/images/Picto_Anime.png" alt="Manga & Anime">
+        <span>Manga & Anime</span>
+      </a>
+    </li>
+    <li>
+      <a href="<?= BASE_URL ?>/?category=7">
+        <img src="public/images/Picto_Internet.png" alt="Internet Culture">
+        <span>Internet Culture</span>
+      </a>
+    </li>
+  </ul>
+</aside>
 
   <main>
       <?= $content ?? 'Default content' ?>
